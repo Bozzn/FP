9fc2ccc2e6b40bd0ca3ef1dce562bbd5cd573945
Piero <pgarciam@gmail.com>
2024-06-24 09:21:43 -0500

    Agregando funcion referencia.cpp

---
+++ referencia.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+void porvalor(int);
+void porReferencia(int);
+
+
+void porvalor(int a){
+    a = 10;
+    cout << "Dentro de porValor, a = " << a << endl;
+
+}
+void porReferencia(int &a){
+    a = 10;
+    cout << "Dentro de porReferencia, a = " << a << endl;
+}
+
+int main(){
+    int num = 5; 
+    cout << "Antes de pasar por valor, num = " << num << endl;
+    porvalor(num);
+    cout << "Luego de pasar por valor, num = " << num << endl;
+
+    cout << "Antes de pasar por referencia, num = " << num << endl;
+    porReferencia(num);
+    cout << "Luego de pasar por referencia, num = " << num << endl;
+}
